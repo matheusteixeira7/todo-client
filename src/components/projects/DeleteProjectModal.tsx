79ba8a7218512a78119860284c6dc1e41fbcf3e1
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-31 03:21:52 -0300

    refactor: change template literals

--- src/components/projects/DeleteProjectModal.tsx
+++ src/components/projects/DeleteProjectModal.tsx
@@ -14,7 +14,7 @@ export const DeleteProjectModal = ({ closeModal, deleteProject }) => {
       <div className="mt-4 md:flex md:items-center md:justify-between">
         <div
           onClick={deleteProject}
-          className={`focus:outline-none group relative mb-4 flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:ring-2 focus:ring-red-500 focus:ring-offset-2 md:mb-0 md:mr-4`}
+          className="focus:outline-none group relative mb-4 flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:ring-2 focus:ring-red-500 focus:ring-offset-2 md:mb-0 md:mr-4"
         >
           <span className="absolute inset-y-0 left-0 flex items-center pl-3">
             <AiFillDelete
@@ -26,7 +26,7 @@ export const DeleteProjectModal = ({ closeModal, deleteProject }) => {
         </div>
         <div
           onClick={closeModal}
-          className={`focus:outline-none group relative flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+          className="focus:outline-none group relative flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         >
           <span className="absolute inset-y-0 left-0 flex items-center pl-3">
             <AiOutlineClose
