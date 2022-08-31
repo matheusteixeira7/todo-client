7bbf90ba235640e80b36263a68c950d47eb41767
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-08-31 16:29:38 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  testEnvironment: 'jsdom',
+}
